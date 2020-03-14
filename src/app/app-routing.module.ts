84a93dead40b93a2d44e91abf6b3051320e2e4e4
dartho2 <wegrzyn_pawel@outlook.com>
2020-03-14 18:07:32 +0100

    rejestracja

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rejestracja',
+    component: RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
